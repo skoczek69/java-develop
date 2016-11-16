c741a30112e2620deaae9f6a98f5598b58d0fab4
skoczek <skoczylas_jacek@o2.pl>
2016-11-16 09:03:29 +0100

    Final version of Bank

--- Bank/src/com/wmiiul/bank/Main.java
+++ Bank/src/com/wmiiul/bank/Main.java
@@ -20,28 +20,28 @@ public class Main {
 		Account hans = ubs.findAccount("54671588785761997433549765");
 
 		jan.doTransaction(TransactionEnum.CHECK, "12345678901234567890123456", 800.0, "Zaplata za monitor");
-		jacek.doTransaction(TransactionEnum.DEPOSIT, "12345678901234567890123456", 800.0, "Zaplata za monitor");
+		jacek.doTransaction(TransactionEnum.DEPOSIT, "11223344556677889900112233", 800.0, "Zaplata za monitor");
 
-		jacek.doTransaction(TransactionEnum.CHECK, "12345678901234567890123456", 450.0, "Zaplata za telefon");
+		jacek.doTransaction(TransactionEnum.CHECK, "11223344556677889900112233", 450.0, "Zaplata za telefon");
 		jan.doTransaction(TransactionEnum.DEPOSIT, "12345678901234567890123456", 450.0, "Zaplata za telefon");
 
 		jan.doTransaction(TransactionEnum.CHECK, "12345678901234567890123456", 300.0, "Zaplata za klawiature");
-		jacek.doTransaction(TransactionEnum.DEPOSIT, "12345678901234567890123456", 300.0, "Zaplata za klawiature");
+		jacek.doTransaction(TransactionEnum.DEPOSIT, "11223344556677889900112233", 300.0, "Zaplata za klawiature");
 
-		jacek.doTransaction(TransactionEnum.CHECK, "12345678901234567890123456", 160.0, "Rekompensata za pomoc");
+		jacek.doTransaction(TransactionEnum.CHECK, "11223344556677889900112233", 160.0, "Rekompensata za pomoc");
 		jan.doTransaction(TransactionEnum.DEPOSIT, "12345678901234567890123456", 160.0, "Rekompensata za pomoc");
 
 		hans.doTransaction(TransactionEnum.WIREOUT, "12345678901234567890123456", 100.0, "Payment for keyboard", "Polska",
 				"ABCDEFGH");
-		jacek.doTransaction(TransactionEnum.DEPOSIT, "12345678901234567890123456", 100.0, "Payment for keyboard");
+		jacek.doTransaction(TransactionEnum.DEPOSIT, "54671588785761997433549765", 100.0, "Payment for keyboard");
 
-		jacek.doTransaction(TransactionEnum.WIREOUT, "12345678901234567890123456", 200.0, "Payment for printer", "Polska",
-				"ABCDEFGH");
+		jacek.doTransaction(TransactionEnum.WIREOUT, "54671588785761997433549765", 200.0, "Payment for printer", "Szwajcaria",
+				"ABCVWXYZ");
 		hans.doTransaction(TransactionEnum.DEPOSIT, "12345678901234567890123456", 200.0, "Payment for printer");
 
 		hans.doTransaction(TransactionEnum.WIREOUT, "12345678901234567890123456", 300.0, "Payment for computer game", "Polska",
 				"ABCDEFGH");
-		jacek.doTransaction(TransactionEnum.DEPOSIT, "12345678901234567890123456", 300.0, "Payment for computer game");
+		jacek.doTransaction(TransactionEnum.DEPOSIT, "54671588785761997433549765", 300.0, "Payment for computer game");
 
 		jacek.amountInfo();
 		jan.amountInfo();
